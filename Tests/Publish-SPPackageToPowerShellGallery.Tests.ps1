38bc99ca8966602c8d256f18c34a35897b9f00ff
Richie Lee <richie@bzzzt.io>
2020-11-05 18:38:33 +0000

    write more tests

--- Tests/Publish-SPPackageToPowerShellGallery.Tests.ps1
+++ Tests/Publish-SPPackageToPowerShellGallery.Tests.ps1
@@ -5,7 +5,25 @@ $seaSalt = Resolve-Path "./SeaSalt"
 
 Describe "Update Module Version Number" {
     it "Can Update Module" {
-        { Publish-SPPackageToPowerShellGallery -apiKey "fakekey" -Path $seaSalt -whatif  } | Should -Not -Throw
+        { Publish-SPPackageToPowerShellGallery -apiKey "fakekey" -Path $seaSalt -whatif } | Should -Not -Throw
     }
 
+    it "Can Update Module" {
+        $PublishModuleParams = @{
+            LicenseUri  = "http://contoso.com/license"
+            Tag         = "Active Directory", "DSC"
+            ReleaseNote = "Updated the ActiveDirectory DSC Resources to support adding users."
+        }
+        { Publish-SPPackageToPowerShellGallery -apiKey "fakekey" -Path $seaSalt -PublishModuleParams $PublishModuleParams -whatif } | Should -Not -Throw
+    }
+    it "Can Publish" {
+        $script:MockPublishModuleCalled = 0
+        $MockPublishModule = {
+            $script:MockPublishModuleCalled++                                
+            return $true
+        }
+        Mock -CommandName Publish-Module -MockWith $MockPublishModule
+        { Publish-SPPackageToPowerShellGallery -apiKey "fakekey" -Path $seaSalt } | Should not Throw
+        Assert-MockCalled Publish-Module -Times $MockPublishModuleCalled
+    }
 }
